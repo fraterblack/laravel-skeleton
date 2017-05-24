22a280f7f631e05650b88942cd29e3bb368ee8eb
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-05-24 16:44:08 -0300

    Remove dirt

--- app/Support/Services/Mail/MailerService.php
+++ app/Support/Services/Mail/MailerService.php
@@ -4,11 +4,7 @@ namespace Lpf\Support\Services\Mail;
 
 use Illuminate\Contracts\Mail\Mailer as MailerContract;
 use Lpf\Support\Services\Mail\Contracts\MailService as MailServiceContract;
-use Lpf\Support\Services\Mail\Traits\Checkout as CheckoutTrait;
-use Lpf\Support\Services\Mail\Traits\Contest as ContestTrait;
 use Lpf\Support\Services\Mail\Traits\General as GeneralTrait;
-use Lpf\Support\Services\Mail\Traits\Profile as ProfileTrait;
-use Lpf\Support\Services\Mail\Traits\Reward as RewardTrait;
 
 class MailerService implements MailServiceContract
 {
