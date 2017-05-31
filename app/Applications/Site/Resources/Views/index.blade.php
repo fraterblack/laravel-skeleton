33471d9df596f79b12031724f477e4661e0ceaff
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-05-31 11:00:21 -0300

    Melhoria UI do header do painel

--- app/Applications/Site/Resources/Views/index.blade.php
+++ app/Applications/Site/Resources/Views/index.blade.php
@@ -4,6 +4,7 @@
 
     <section>
         <p>Página inicial da aplicação Site</p>
+        <p>Para acessar o painel administrativo, acesse: <a href="{{ route('admin.initial') }}">{{ env('ADMIN_URL') }}</a></p>
     </section>
 
 @endsection
