2dd1d2e8c26aeabe925334b34c3f52360da15d43
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-07-27 15:44:58 -0300

    Melhorias e documentação

--- app/Support/Helpers/NumberFormatter.php
+++ app/Support/Helpers/NumberFormatter.php
@@ -2,18 +2,45 @@
 
 namespace Lpf\Support\Helpers;
 
+/**
+ * Class NumberFormatter.
+ *
+ * Helps format a number.
+ */
 class NumberFormatter
 {
-    static public function toFloat($number, $decimals = 2, $thousandsSeparator = '')
+    /**
+     * Formata número para float
+     * @param mixed $number
+     * @param null|integer $decimals
+     * @param string $thousandsSeparator
+     * @return float|string
+     */
+    static public function toFloat($number, $decimals = null, $thousandsSeparator = '')
     {
+        if (! $decimals) {
+            return self::prepareNumberToFormat($number);
+        }
+
         return number_format(self::prepareNumberToFormat($number), $decimals, '.', $thousandsSeparator);
     }
 
+    /**
+     * Formata número para o formato real
+     * @param mixed $number
+     * @param int $decimals
+     * @param string $thousandsSeparator
+     * @return string
+     */
     static public function toReal($number, $decimals = 2, $thousandsSeparator = '')
     {
         return number_format(self::prepareNumberToFormat($number), $decimals, ',', $thousandsSeparator);
     }
 
+    /**
+     * @param mixed $number
+     * @return float
+     */
     static protected function prepareNumberToFormat($number)
     {
         if (self::isRealFormat($number)) {
@@ -23,6 +50,10 @@ class NumberFormatter
         }
     }
 
+    /**
+     * @param mixed $number
+     * @return bool
+     */
     static public function isRealFormat($number)
     {
         if (preg_match('/([0-9,]+\,[0-9]{2})$/', $number)) {
