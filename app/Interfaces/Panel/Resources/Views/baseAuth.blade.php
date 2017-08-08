07d879a13d5c9d35cfa118a1eda4f1f609189cbb
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-08-08 15:02:25 -0300

    Remove logo duplicada na tela de login

--- app/Interfaces/Panel/Resources/Views/baseAuth.blade.php
+++ app/Interfaces/Panel/Resources/Views/baseAuth.blade.php
@@ -5,9 +5,6 @@
     @include('panel::_foot')
 
         <div class="login-box">
-            <div class="login-logo">
-                <a href="{{ config('app.url') }}"><img src="{{ config('app.panel.contractor.logo') }}" alt="{{ config('app.panel.contractor.name') }}"></a>
-            </div>
             @yield('contentWrapper')
         </div>
         <div id="pageLoading" class="page-loading" data-toggle="tooltip" title="Carregando. Aguarde..."></div>
