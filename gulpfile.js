52f649519f86a4f51365ef79e5a511f917b575aa
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-08-16 11:23:04 -0300

    Criado checagem do prefixo da task para evitar conflitos com tasks de outras interfaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 // Tasks RequireJs
 var gulp = require('gulp'),
-    requireDir = require('require-dir');
+    requireDir = require('require-dir'),
+    argv = require('yargs').argv;
+
+/*
+* README
+* Tasks da interface panel estão contidas no diretório /gulp-panel e desta maneira podem ser isoladas de tasks de outras interfaces
+* Por convenção tasks do painel devem ser prefixadas com "panel" para garantir que não aconteça conflitos com tasks de outras interfaces.
+* Assim as tasks ficam nomeadas como panel-libs, panel-scripts, panel-styles, panel-sass, panel-watch
+*/
+
+//Verifica o prefixo da tarefa para carregar as tasks específicas do panel caso seja o caso
+for (var i in argv._) {
+    if (argv._[i].match(/^panel-/i)) {
+        //Tasks do Painel
+        requireDir('./gulp-panel', { recurse: true });
+
+        break;
+    }
+}
 
 //Tasks do Site
 //requireDir('./gulp', { recurse: true });
-
-//Tasks do Painel
-requireDir('./gulp-panel', { recurse: true });
